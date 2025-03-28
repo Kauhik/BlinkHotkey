bb0a39abab424deb225ebd4133df5598861dd8fc
Kaushik <69705545+Kauhik@users.noreply.github.com>
2025-03-28 19:33:34 +0800

    Initial Commit

---
+++ BlinkHotkeyTests/BlinkHotkeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkHotkeyTests.swift
+//  BlinkHotkeyTests
+//
+//  Created by Kaushik Manian on 28/3/25.
+//
+
+import Testing
+@testable import BlinkHotkey
+
+struct BlinkHotkeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
